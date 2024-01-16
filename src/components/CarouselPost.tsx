c8d24cb2074234f0fa89356f9fc867e1f6858837
ertiti11 <vatitiprieto11@gmail.com>
2024-01-16 14:53:25 +0100

    added shadcn carousel

---
+++ src/components/CarouselPost.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function CarouselPost({className}) {
+  return (
+    <div className={`w-32 h-32 bg-black ${className}`}>
+        <h1>hola que ase</h1>
+    </div>
+  )
+}
---
+++ src/components/FeaturedCarousel.jsx
@@ -0,0 +1,32 @@
+
+import { Card, CardContent } from "./ui/card"
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "./ui/carousel"
+import CarouselPost from "./CarouselPost"
+
+export default function FeaturedCarousel() {
+  return (
+    <Carousel className="w-full max-w-xs">
+      <CarouselContent>
+        {Array.from({ length: 5 }).map((_, index) => (
+          <CarouselItem key={index}>
+            <div className="p-1">
+              <Card>
+                <CardContent className="flex aspect-square items-center justify-center p-6">
+                  <CarouselPost className=" text-left w-full  text-white"/>
+                </CardContent>
+              </Card>
+            </div>
+          </CarouselItem>
+        ))}
+      </CarouselContent>
+      <CarouselPrevious />
+      <CarouselNext />
+    </Carousel>
+  )
+}
