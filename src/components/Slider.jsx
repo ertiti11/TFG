218c1ebc2f2ca7d5c004286631e9b784e65d85eb
ertiti11 <vatitiprieto11@gmail.com>
2024-03-27 18:48:21 +0100

    slider Creado

---
+++ src/components/Slider.jsx
@@ -0,0 +1,52 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+import {
+  Navigation,
+  Pagination,
+  Scrollbar,
+  Autoplay,
+  EffectFade,
+} from "swiper/modules";
+import "swiper/css";
+import "swiper/css/bundle";
+export default function Slider() {
+  return (
+    <Swiper
+    style={{ width: "1245px", borderRadius: "20px", marginTop: "-30px", position: "relative"}}
+    className=" w-[1245px]"
+      modules={[Navigation, Pagination, Scrollbar, EffectFade, Autoplay]}
+      spaceBetween={50}
+      slidesPerView={1}
+      navigation
+      effect="fade"
+      loop={true}
+      crossFade={true}
+      pagination={{ clickable: true }}
+      scrollbar={{ draggable: true }}
+      autoplay={{ delay: 3000 }}
+      onSlideChange={() => console.log("slide change")}
+      onSwiper={(swiper) => console.log(swiper)}
+    >
+      <SwiperSlide>
+        <img
+          src="https://escuela.fundacioncontadorteam.com/wp-content/uploads/slide-01-2048x1050.jpg"
+          alt=""
+          loading="lazy"
+        />
+      </SwiperSlide>
+      <SwiperSlide>
+        <img
+          src="https://escuela.fundacioncontadorteam.com/wp-content/uploads/slide-02-2048x1050.jpg"
+          alt=""
+          loading="lazy"
+        />
+      </SwiperSlide>
+      <SwiperSlide>
+        <img
+          src="https://escuela.fundacioncontadorteam.com/wp-content/uploads/slide-01-2048x1050.jpg"
+          alt=""
+          loading="lazy"
+        />
+      </SwiperSlide>
+    </Swiper>
+  );
+}
