c628254df6dc5ea934f163477dbcc0d73ab10d9a
ertiti11 <vatitiprieto11@gmail.com>
2024-03-28 19:54:58 +0100

    seccion de ultimos posts termiando, falta conectar con backend

--- src/components/NewPosts/Post.jsx
+++ src/components/NewPosts/Post.jsx
@@ -1,30 +1,27 @@
 import React from "react";
 
-export default function Post() {
+export default function Post({ title, date, category, thumbnail }) {
   return (
     <div className="w-96">
       <div className=" w-96 rounded-lg h-64 relative">
         <div className=" rounded-xl absolute inset-0 bg-gradient-to-t from-black to-transparent opacity-70 z-10 "></div>
         <img
           className="w-full h-full rounded-xl object-cover"
-          src="https://www.ciclismoafondo.es/uploads/s1/12/52/84/6/5dc6b0680de694c7403493c1-10-ciclistas-a-seguir-en-2020.jpeg"
+            src={thumbnail}
           alt=""
         />
         <div className="absolute bottom-0 z-10 text-white py-4 px-3 flex items-center gap-2 justify-between w-full">
           <div className="flex gap-2">
             <div className="w-20 py-1 px-6 bg-blue-500 flex items-center justify-center rounded-full leading-normal ">
-              <span className="m-0 p-0 ">cosas</span>
-            </div>
-            <div className="w-20 py-1 px-6 bg-blue-500 flex items-center justify-center rounded-full leading-normal ">
-              <span className="m-0 p-0 ">cosas</span>
+              <span className="m-0 p-0 ">{category}</span>
             </div>
           </div>
 
-          <date>12/12/2003</date>
+          <date>{date}</date>
         </div>
       </div>
       <h3 className=" text-3xl font-bold">
-        Como ser mejor persona sin parecerlo
+        {title}
       </h3>
     </div>
   );
--- src/components/NewPosts/PostList.jsx
+++ src/components/NewPosts/PostList.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import PocketBase from "pocketbase";
 import Post from "./Post";
+import BigPost from "./BigPost";
 export default function PostList() {
   const [posts, setPosts] = useState([]);
 
@@ -15,8 +16,42 @@ export default function PostList() {
   }, []);
 
   return (
-    <>
-      <Post/>
-    </>
+    <section className="w-[1245px] mt-24">
+        <h1 className="text-center font-bold text-5xl">Nuevas Noticias</h1>
+      <BigPost
+        title="como ser mejor persona"
+        category={"carrera"}
+        date="12/12/2024"
+        thumbnail={
+          "https://www.ciclismoafondo.es/uploads/s1/12/52/84/6/5dc6b0680de694c7403493c1-10-ciclistas-a-seguir-en-2020.jpeg"
+        }
+      />
+      <div className="flex justify-between mt-10">
+        <Post
+          title="como ser mejor persona"
+          category={"carrera"}
+          date="12/12/2024"
+          thumbnail={
+            "https://www.ciclismoafondo.es/uploads/s1/12/52/84/6/5dc6b0680de694c7403493c1-10-ciclistas-a-seguir-en-2020.jpeg"
+          }
+        />
+        <Post
+          title="como ser mejor persona"
+          category={"carrera"}
+          date="12/12/2024"
+          thumbnail={
+            "https://www.ciclismoafondo.es/uploads/s1/12/52/84/6/5dc6b0680de694c7403493c1-10-ciclistas-a-seguir-en-2020.jpeg"
+          }
+        />
+        <Post
+          title="como ser mejor persona"
+          category={"carrera"}
+          date="12/12/2024"
+          thumbnail={
+            "https://www.ciclismoafondo.es/uploads/s1/12/52/84/6/5dc6b0680de694c7403493c1-10-ciclistas-a-seguir-en-2020.jpeg"
+          }
+        />
+      </div>
+    </section>
   );
 }
