494c5f180451109f1c84211daf31060e03d912b4
ertiti11 <vatitiprieto11@gmail.com>
2024-03-31 00:50:51 +0100

    creado archivo de utiles

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,37 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+function createSlug(str) {
+  const map = {
+    á: "a",
+    é: "e",
+    í: "i",
+    ó: "o",
+    ú: "u",
+    Á: "A",
+    É: "E",
+    Í: "I",
+    Ó: "O",
+    Ú: "U",
+  };
+
+  str = str.replace(/[áéíóúÁÉÍÓÚ]/g, (match) => map[match]);
+
+  return str
+    .toLowerCase()
+    .replace(/[^a-z0-9 -]/g, "") // quita caracteres inválidos
+    .replace(/\s+/g, "-") // reemplaza espacios por guiones
+    .replace(/-+/g, "-"); // reemplaza múltiples guiones por uno solo
+}
+
+
+
+
+function getThumb(post) {
+  return `https://esciclismomalaga.pockethost.io/api/files/${post.collectionId}/${post.id}/${post.thumbnail}`
+  
 }
