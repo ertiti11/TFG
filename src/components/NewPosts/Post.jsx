c2f6fe982825fe64f503c2a53e444dc10737254a
Diego <diegoalarcondrum@gmail.com>
2024-03-30 21:15:20 +0100

    paths de blog creados

--- src/components/NewPosts/Post.jsx
+++ src/components/NewPosts/Post.jsx
@@ -1,13 +1,20 @@
 
+function createSlug(str) {
+  return str
+    .toLowerCase()
+    .replace(/[^a-z0-9 -]/g, '')  // quita caracteres inválidos
+    .replace(/\s+/g, '-')         // reemplaza espacios por guiones
+    .replace(/-+/g, '-');         // reemplaza múltiples guiones por uno solo
+}
 
 export default function Post({ title, date, category, thumbnail }) {
   return (
-    <div className="w-96">
+    <a className="w-96" href={`/blog/${createSlug(title)}`}>
       <div className=" w-96 rounded-lg h-64 relative">
         <div className=" rounded-xl absolute inset-0 bg-gradient-to-t from-black to-transparent opacity-70 z-10 "></div>
         <img
           className="w-full h-full rounded-xl object-cover"
-            src={thumbnail}
+          src={thumbnail}
           alt=""
         />
         <div className="absolute bottom-0 z-10 text-white py-4 px-3 flex items-center gap-2 justify-between w-full">
@@ -23,6 +30,6 @@ export default function Post({ title, date, category, thumbnail }) {
       <h3 className=" text-3xl font-bold">
         {title}
       </h3>
-    </div>
+    </a>
   );
 }
